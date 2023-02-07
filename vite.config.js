dbe45e11121247717576b77ba537e874e03df417
Duc Le <duc.lv@primecommerce.edu.vn>
2023-02-07 12:39:01 +0700

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), crx({ manifest })],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
